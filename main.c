dbc68939f7bb8e29e5a3601c4329724f20bb50aa
rgroener <username@users.noreply.github.com>
2019-05-09 15:50:51 +0000

    simple formatted text

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ Nokia Sensorboard
 * => read / write RTC (I2C)
 * => logging function
 * 
-* 
+* z
 */
 
 
@@ -56,25 +56,18 @@ uint8_t sekunden, sekunden_alt, minuten, stunden;
 volatile uint8_t 	received_byte;
 int32_t temp, press, hum;
 uint16_t vork, nachk; 
-
 uint8_t flag_send, flag_send_index;
 // String für Zahlenausgabe
-char string[30] = "";
-
+char string[40] = "";
 uint8_t ms, ms10,ms100,sec,min,entprell, state;
 uint8_t end_ms100, end_sec, end_min;
 enum state{WAIT, COUNT, TIME, TIME_WAIT,FLIGHT_TIME};
+uint8_t str_len;
 
-
-
-
-
-
-
-
-
-#define DEBUG 1
-
+static uint8_t read_line(char* buffer, uint8_t buffer_length);
+static uint32_t strtolong(const char* str);
+static uint8_t find_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, const char* name, struct fat_dir_entry_struct* dir_entry);
+static struct fat_file_struct* open_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, const char* name); 
 static uint8_t print_disk_info(const struct fat_fs_struct* fs);
 
 /* Function prototypes */
@@ -113,39 +106,23 @@ int main(void)
 	DDRB |= (1<<PB2);
 	PORTB |= (1<<PB2);
 	//PORTB &= ~(1<<PB2);
-	
-	
 	DDRC |=(1<<PC3); 	//Ausgang LED
 	PORTC |= (1<<PC3);	//Led ein
-	
-	
 	DDRD &= ~((1<<PD6) | (1<<PD2) | (1<<PD5)); 	//Taster 1-3
 	PORTD |= ((1<<PD6) | (1<<PD2) | (1<<PD5)); 	//PUllups für Taster einschalten
-	
 	DDRD &= ~(1<<PD4); //T0 Counter Input
 	TCCR0B |= (1<<CS02) | (1<<CS01) | (1<<CS00);//Counter 0 enabled clock on rising edge
-	
 	//Timer 1 Configuration
 	OCR1A = 0x009C;	//OCR1A = 0x3D08;==1sec
-	
-    TCCR1B |= (1 << WGM12);
+	TCCR1B |= (1 << WGM12);
     // Mode 4, CTC on OCR1A
-
     TIMSK1 |= (1 << OCIE1A);
     //Set interrupt on compare match
-
     TCCR1B |= (1 << CS12) | (1 << CS10);
     // set prescaler to 1024 and start the timer
-
-
-
     sei();
     // enable interrupts
-	
 	setup();
-	
-	
-	
 	
 	zaehler=222;
 	entprell=0;
@@ -174,13 +151,11 @@ int main(void)
 	vork =0;
 	flag_send=0;
 	flag_send_index=0;
+	str_len=0;//length of a string
 
-	
 		glcd_tiny_set_font(Font5x7,5,7,32,127);
 		glcd_clear_buffer();
-		
-	
-  
+
 	BME280_init();
    BME280_set_filter(BME280_FILTER_16);
 	BME280_set_standby(BME280_TSB_10);
@@ -192,54 +167,80 @@ int main(void)
  		
  	
 		glcd_clear();
-		  /* setup uart */
-    uart_init();
-		     // setup sd card slot
-        if(!sd_raw_init())
-        {
-           glcd_draw_string_xy(0,0,"Init fail...");
-        }
+		/* setup uart */
+		uart_init();
+		// setup sd card slot
+		if(!sd_raw_init())
+		{
+			glcd_draw_string_xy(0,0,"Init fail...");
+		}
 
-	 // open first partition 
-        struct partition_struct* partition = partition_open(sd_raw_read,sd_raw_read_interval,sd_raw_write,sd_raw_write_interval,0);
-                                                           
+		// open first partition 
+		struct partition_struct* partition = partition_open(sd_raw_read,sd_raw_read_interval,sd_raw_write,sd_raw_write_interval,0);
+
 		if(!partition)
 		{
-           // If the partition did not open, assume the storage device
-             // is a "superfloppy", i.e. has no MBR.
-             
-            partition = partition_open(sd_raw_read,sd_raw_read_interval,sd_raw_write,sd_raw_write_interval,-1);
-            if(!partition)
-            {
+			// If the partition did not open, assume the storage device
+			// is a "superfloppy", i.e. has no MBR.
+			partition = partition_open(sd_raw_read,sd_raw_read_interval,sd_raw_write,sd_raw_write_interval,-1);
+			if(!partition)
+			{
 				glcd_draw_string_xy(0,0,"part failed...");
-            }
-        }                                               
-     
+			}
+		} 
 		 /* open file system */
-        struct fat_fs_struct* fs = fat_open(partition);
-        if(!fs)
-        {
-            glcd_draw_string_xy(0,0,"fs failed...");
-        }
+		struct fat_fs_struct* fs = fat_open(partition);
+		if(!fs)
+		{
+			glcd_draw_string_xy(0,0,"fs failed...");
+		}
 
-        /* open root directory */
-        struct fat_dir_entry_struct directory;
-        fat_get_dir_entry_of_path(fs, "/", &directory);
+		/* open root directory */
+		struct fat_dir_entry_struct directory;
+		fat_get_dir_entry_of_path(fs, "/", &directory);
 
-        struct fat_dir_struct* dd = fat_open_dir(fs, &directory);
-        if(!dd)
-        {
-            glcd_draw_string_xy(0,0,"root failed...");
-        }
-        
-         /* print some card information as a boot message */
-        print_disk_info(fs);
+		struct fat_dir_struct* dd = fat_open_dir(fs, &directory);
+		if(!dd)
+		{
+			glcd_draw_string_xy(0,0,"root failed...");
+		}
+
+		/* print some card information as a boot message */
+		print_disk_info(fs);
+		
+		struct fat_file_struct* fd = open_file_in_dir(fs, dd, "test.txt");
+		if(!fd)
+		{
+			uart_puts("could not open test.txt\n");
+		}
+		else
+		{
+			sprintf(string,"roman groener");
+			str_len=strlen(string);
+			fat_write_file(fd,string,str_len);
+			sd_raw_sync();
+			sprintf(string,"\nSensor 1  %d; Sensor 2  %d; Sensor 3  %d", 11,22,3);
+			str_len=strlen(string);
+			fat_write_file(fd,string,str_len);
+			sd_raw_sync();
+			sprintf(string,"\nNeue Zeile");
+			str_len=strlen(string);
+			fat_write_file(fd,string,str_len);
+			sd_raw_sync();
+			uart_puts("writing sucessfull\n");
+		}
+		fat_close(fs);
+		partition_close(partition);
+		
+		
+		
+		
 		while(1) 
 		{	
 		
 		  
-	
-	glcd_draw_string_xy(0,0,"SD success...");
+	sprintf(string,"string %d",str_len);
+	glcd_draw_string_xy(0,0,string);
 
 		/*
 			BME280_readout(&temp, &press, &hum);
@@ -337,6 +338,79 @@ int main(void)
 	return 0;
 }//end of main
 
+uint8_t read_line(char* buffer, uint8_t buffer_length)
+{
+    memset(buffer, 0, buffer_length);
+
+    uint8_t read_length = 0;
+    while(read_length < buffer_length - 1)
+    {
+        uint8_t c = uart_getc();
+
+        if(c == 0x08 || c == 0x7f)
+        {
+            if(read_length < 1)
+                continue;
+
+            --read_length;
+            buffer[read_length] = '\0';
+
+            uart_putc(0x08);
+            uart_putc(' ');
+            uart_putc(0x08);
+
+            continue;
+        }
+
+        uart_putc(c);
+
+        if(c == '\n')
+        {
+            buffer[read_length] = '\0';
+            break;
+        }
+        else
+        {
+            buffer[read_length] = c;
+            ++read_length;
+        }
+    }
+
+    return read_length;
+}
+
+uint32_t strtolong(const char* str)
+{
+    uint32_t l = 0;
+    while(*str >= '0' && *str <= '9')
+        l = l * 10 + (*str++ - '0');
+
+    return l;
+}
+
+uint8_t find_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, const char* name, struct fat_dir_entry_struct* dir_entry)
+{
+    while(fat_read_dir(dd, dir_entry))
+    {
+        if(strcmp(dir_entry->long_name, name) == 0)
+        {
+            fat_reset_dir(dd);
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+struct fat_file_struct* open_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, const char* name)
+{
+    struct fat_dir_entry_struct file_entry;
+    if(!find_file_in_dir(fs, dd, name, &file_entry))
+        return 0;
+
+    return fat_open_file(fs, &file_entry);
+}
+
 uint8_t print_disk_info(const struct fat_fs_struct* fs)
 {
     if(!fs)
@@ -363,3 +437,17 @@ uint8_t print_disk_info(const struct fat_fs_struct* fs)
 
     return 1;
 }
+
+
+void get_datetime(uint16_t* year, uint8_t* month, uint8_t* day, uint8_t* hour, uint8_t* min, uint8_t* sec)
+{
+    *year = 2007;
+    *month = 1;
+    *day = 1;
+    *hour = 0;
+    *min = 0;
+    *sec = 0;
+}
+
+
+
